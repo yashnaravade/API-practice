3c697b42df96c238ef332375c8b49801a384fbda
Yash Naravade <yashdip123@gmail.com>
2022-07-13 17:08:12 +0530

    added folder for routes

--- backend/server.js
+++ backend/server.js
@@ -1,52 +1,13 @@
-const express = require("express");
-const dotenv = require("dotenv").config();
-const port = process.env.PORT || 5000;
+const express = require("express"); // Import express module
+const dotenv = require("dotenv").config(); // Import dotenv module
+const port = process.env.PORT || 5000; // Set port to 5000 or the port defined in the .env file
 
-const app = express();
+const app = express(); // create an instance of express
 
-app.get("/api/goals", (req, res) => {
-    res.status(200).json({message: "Get Goals!"});
-});
-
-// .listen(port, () => {
-//     console.log(`Server started on port ${port}`);
-// }).on("error", (err) => {
-//     console.log(err);
-// }).on("close", () => {
-//     console.log("Server closed");
-// }).on("listening", () => {
-//     console.log("Server listening");
-// }).on("connection", () => {
-//     console.log("Connection established");
-// }).on("request", () => {
-//     console.log("Request received");
-// }).on("response", () => {
-//     console.log("Response sent");
-// }).on("disconnect", () => {
-//     console.log("Connection disconnected");
-// }).on("close", () => {
-//     console.log("Connection closed");
-// }).on("connect", () => {
-//     console.log("Connection established");
-// }).on("upgrade", () => {
-//     console.log("Upgrade request received");
-// }).on("end", () => {
-//     console.log("Connection ended");
-// }).on("readable", () => {
-//     console.log("Connection readable");
-// }).on("drain", () => {
-//     console.log("Connection drained");
-// }).on("pipe", () => {
-//     console.log("Connection piped");
-// }).on("unpipe", () => {
-//     console.log("Connection unpiped");
-// }).on("finish", () => {
-//     console.log("Connection finished");
-// })
+app.use("/api/goals", require("./routes/goalRoutes")); // require the goalRoutes.js file and use it for the /api/goals route
 
 
-app.listen(port, () => {
-    console.log(`Server started on port ${port}`);
-    }
+app.listen(port, () => { // listen on port 5000
+    console.log(`Server started on port ${port}`); // log to the console that the server has started
+    } 
 );
-
