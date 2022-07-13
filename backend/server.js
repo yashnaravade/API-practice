40522a58256671710f633323de5dc5d9d8d2bb5e
Yash Naravade <yashdip123@gmail.com>
2022-07-13 16:05:53 +0530

    added status 200

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 5000;
 const app = express();
 
 app.get("/api/goals", (req, res) => {
-    res.json({message: "Get Goals!"});
+    res.status(200).json({message: "Get Goals!"});
 });
 
 // .listen(port, () => {
